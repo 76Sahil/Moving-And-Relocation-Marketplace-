db910b0d396914f8e59d669781ebf660867e003d
sahil <sahilmpaliwal@gmail.com>
2026-09-08 00:09:30 +0530

    initialize Project Structure

---
+++ src/main/java/com/movingmarketplace/MovingRelocationBackendApplication.java
@@ -0,0 +1,13 @@
+package com.movingmarketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovingRelocationBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovingRelocationBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/movingmarketplace/MovingRelocationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movingmarketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovingRelocationBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
